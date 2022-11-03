e019319842c2aa35bf188a876acad7832018729b
Suz <duonghuy14102000@gmail.com>
2022-11-03 19:00:51 +0700

    Add BMP280 sensor. RTOS task debounce interrupt button. Schedule ping master board limiter.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
+#include <Adafruit_BMP280.h>
 #include <Arduino.h>
 #include <WiFi.h>
 #include <esp_now.h>
 #include <esp_wifi.h>
 
 #define BOARD_ID 3
-#define SWITCH_PIN 26
-#define DEVICE_PIN 25
+#define SWITCH_PIN 19
+#define detectPin 23
+#define DEVICE_PIN 5
+#define FAILED_LIMIT 10
+#define DEBOUNCETIME 150
+
+volatile int numberOfButtonInterrupts = 0;
+volatile bool lastState;
+volatile uint32_t debounceTimeout = 0;
+
+Adafruit_BMP280 bmp;  // I2C
 
 unsigned long previousMillis = 0;  // Stores last time temperature was published
 const long interval = 20000;  // Interval at which to publish sensor readings
+
+unsigned int readingId = 0;
 
 bool DeviceState = false;
 bool SendedStatus = false;
@@ -22,25 +34,116 @@ typedef struct Device_Status {
     byte id : 4;
     byte WiFi_Channel;
     bool status;
+    int temp;
+    int readingID;
 } Device_Status;
 
 Device_Status myData;
+byte Failed_Count = 0;
+
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+
+void IRAM_ATTR handleButtonInterrupt() {
+    portENTER_CRITICAL_ISR(&mux);
+    numberOfButtonInterrupts++;
+    lastState = digitalRead(SWITCH_PIN);
+    debounceTimeout =
+        xTaskGetTickCount();  // version of millis() that works from interrupt
+    portEXIT_CRITICAL_ISR(&mux);
+}
+
+void taskButtonRead(void *parameter) {
+    String taskMessage = "Debounced ButtonRead Task running on core ";
+    taskMessage = taskMessage + xPortGetCoreID();
+    Serial.println(taskMessage);
+
+    // set up button Pin
+    pinMode(SWITCH_PIN, INPUT_PULLUP);  // Pull up to 3.3V on input - some
+    // buttons already have this done
+
+    attachInterrupt(digitalPinToInterrupt(SWITCH_PIN), handleButtonInterrupt,
+                    FALLING);
+
+    uint32_t saveDebounceTimeout;
+    bool saveLastState;
+    int save;
+
+    // Enter RTOS Task Loop
+    while (1) {
+        portENTER_CRITICAL_ISR(
+            &mux);  // so that value of numberOfButtonInterrupts,l astState are
+        // atomic - Critical Section
+        save = numberOfButtonInterrupts;
+        saveDebounceTimeout = debounceTimeout;
+        saveLastState = lastState;
+        portEXIT_CRITICAL_ISR(&mux);  // end of Critical Section
+
+        bool currentState = digitalRead(SWITCH_PIN);
+
+        // This is the critical IF statement
+        // if Interrupt Has triggered AND Button Pin is in same state AND the
+        // debounce time has expired THEN you have the button push!
+        //
+        if ((save != 0)                         // interrupt has triggered
+            && (currentState == saveLastState)  // pin is still in the same
+            // state as when intr triggered
+            && (millis() - saveDebounceTimeout >
+                DEBOUNCETIME)) {  // and it has been low for at least
+            // DEBOUNCETIME, then valid keypress
+
+            if (currentState == LOW) {
+                DeviceState = !DeviceState;
+                digitalWrite(DEVICE_PIN, DeviceState);
+
+                // Serial.printf(
+                //     "Button is pressed and debounced, current tick=%d\n",
+                //     millis());
+            } else {
+                Serial.printf(
+                    "Button is released and debounced, current tick=%d\n",
+                    millis());
+            }
+
+            // Serial.printf(
+            //     "Button Interrupt Triggered %d times, current State=%u, time
+            //     " "since last trigger %dms\n", save, currentState, millis() -
+            //     saveDebounceTimeout);
+
+            portENTER_CRITICAL_ISR(
+                &mux);  // can't change it unless, atomic - Critical section
+            numberOfButtonInterrupts =
+                0;  // acknowledge keypress and reset interrupt counter
+            portEXIT_CRITICAL_ISR(&mux);
+
+            vTaskDelay(10 / portTICK_PERIOD_MS);
+        }
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+}
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-    Serial.print("\r\nLast Packet Send Status:\t");
-    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success"
-                                                  : "Delivery Fail");
+    Serial.print("Last Packet Send Status:\t");
+    Serial.print(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success\n"
+                                                : "Delivery Fail\n");
 
     if (status != ESP_NOW_SEND_SUCCESS) {
-        Serial.println("Raising flag");
-        Slave_On_Correct_Channel = false;
-    } else
+        Failed_Count++;
+        if (Slave_On_Correct_Channel) {
+            Serial.println("Raising flag");
+            Slave_On_Correct_Channel = false;
+        }
+    } else {
+        Failed_Count = 0;
         Slave_On_Correct_Channel = true;
+    }
 }
 
 void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len) {
-    char macStr[18];
+    Failed_Count = 0;
     Slave_On_Correct_Channel = true;
+    
+    char macStr[18];
     Serial.print("Packet received from: ");
     snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
              mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4],
@@ -56,6 +159,7 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len) {
             Serial.println("Request to turn off");
             DeviceState = false;
         }
+        digitalWrite(DEVICE_PIN, DeviceState);
     }
 }
 
@@ -83,44 +187,9 @@ void initPeers() {
     }
 }
 
-// bool ManageConnection() {
-//     if (!esp_now_is_peer_exist(peerInfo.peer_addr)) {
-//         Serial.println("No connection, attempt repair");
-//         esp_wifi_set_promiscuous(true);
-//         esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
-//         esp_wifi_set_promiscuous(false);
-//         // Station not paired, attempt pair
-//         esp_err_t addStatus = esp_now_add_peer(&peerInfo);
-//         if (addStatus == ESP_OK) {
-//             // Pair success
-//             Serial.println("Pair success");
-//             return true;
-//         } else if (addStatus == ESP_ERR_ESPNOW_NOT_INIT) {
-//             // How did we get so far!!
-//             Serial.println("ESPNOW Not Init");
-//             return false;
-//         } else if (addStatus == ESP_ERR_ESPNOW_ARG) {
-//             Serial.println("Invalid Argument");
-//             return false;
-//         } else if (addStatus == ESP_ERR_ESPNOW_FULL) {
-//             Serial.println("Peer list full");
-//             return false;
-//         } else if (addStatus == ESP_ERR_ESPNOW_NO_MEM) {
-//             Serial.println("Out of memory");
-//             return false;
-//         } else if (addStatus == ESP_ERR_ESPNOW_EXIST) {
-//             Serial.println("Peer Exists");
-//             return true;
-//         } else {
-//             Serial.println("Not sure what happened");
-//             return false;
-//         }
-//     }
-// }
-
 void Channeling_Monitor() {
     while (!Slave_On_Correct_Channel) {
-        Serial.printf("Current channel %d. Switch to channel default(0)\n", peerInfo.channel);
+        Serial.printf("Switch to channel default(0)\n");
         esp_wifi_set_promiscuous(true);
         esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
         esp_wifi_set_promiscuous(false);
@@ -150,11 +219,37 @@ void SendData() {
 void setup() {
     Serial.begin(115200);
     WiFi.mode(WIFI_STA);
-    pinMode(SWITCH_PIN, INPUT_PULLUP);
     pinMode(DEVICE_PIN, OUTPUT);
+    pinMode(detectPin, INPUT_PULLUP);
+
+    xTaskCreatePinnedToCore(taskButtonRead,
+                            "TaskButton"  // A name just for humans
+                            ,
+                            2048  // This stack size can be checked & adjusted
+                                  // by reading the Stack Highwater
+                            ,
+                            NULL,
+                            1  // Priority, with 3 (configMAX_PRIORITIES - 1)
+                               // being the highest, and 0 being the lowest.
+                            ,
+                            NULL, ARDUINO_RUNNING_CORE);
 
     initESPNOW();
     initPeers();
+
+    if (!bmp.begin(0x76)) {
+        Serial.println(
+            "Could not find a valid BMP280 sensor, check wiring or "
+            "try a different address!");
+        while (1) delay(10);
+    }
+
+    /* Default settings from datasheet. */
+    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,  /* Operating Mode. */
+                    Adafruit_BMP280::SAMPLING_X2,  /* Temp. oversampling */
+                    Adafruit_BMP280::SAMPLING_X16, /* Pressure oversampling */
+                    Adafruit_BMP280::FILTER_X16,   /* Filtering. */
+                    Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 }
 
 void loop() {
@@ -165,12 +260,7 @@ void loop() {
         myData.id = BOARD_ID;
     }
 
-    if (!digitalRead(SWITCH_PIN)) {
-        DeviceState = !DeviceState;
-        delay(200);
-    }
-
-    digitalWrite(DEVICE_PIN, DeviceState);
+    myData.status = digitalRead(detectPin) ? false : true;
 
     if (DeviceState != SendedStatus) {
         myData.id = BOARD_ID;
@@ -187,8 +277,27 @@ void loop() {
         previousMillis = currentMillis;
         // Set values to send
         myData.id = BOARD_ID;
-        SendData();
-        SendedStatus = myData.status;
-        Serial.println("Scheduled ping");
+        myData.temp = bmp.readTemperature() * 100;
+        myData.readingID = readingId++;
+        if (Failed_Count <= FAILED_LIMIT) {
+            Serial.println("Scheduled ping");
+            SendData();
+            SendedStatus = myData.status;
+        }
+
+        Serial.print(F("Temperature = "));
+        Serial.print(bmp.readTemperature());
+        Serial.println(" *C");
+
+        Serial.print(F("Pressure = "));
+        Serial.print(bmp.readPressure());
+        Serial.println(" Pa");
+
+        Serial.print(F("Approx altitude = "));
+        Serial.print(
+            bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+        Serial.println(" m");
+
+        Serial.println();
     }
 }
